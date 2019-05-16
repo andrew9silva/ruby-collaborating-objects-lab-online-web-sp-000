f1ce0ab6659416aa22a8bcd1013dbe2b9c29d00a
Andrew Silva <andrewsilva9@gmail.com>
2019-05-16 02:17:42 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,4 +5,8 @@ class MP3Importer
   def initialize(path)
     @path = path
   end
+
+  def files
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+  end
 end
