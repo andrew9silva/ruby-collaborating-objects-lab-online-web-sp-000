e9f613e9dea4e3421c54e4174fbb761d731b84cd
Andrew Silva <andrewsilva9@gmail.com>
2019-05-16 02:04:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,6 +20,7 @@ class Song
     artist = filename.split(" - ")[0]
     new_song = self.new(song)
     new_song.artist = Artist.find_or_create_by_name(artist)
+    new_song.artist = 
     new_song
   end
 end
